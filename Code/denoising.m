4f25767c3fc91163dd05fea07a526a3f987f2e4e
Pierrick ULLIUS <pierrick.ullius@gmail.com>
2021-01-02 15:22:26 +0100

    Output : denoised images

--- Code/denoising.m
+++ Code/denoising.m
@@ -1,91 +1,110 @@
 clear all;
 close all;
 
+
 %Remove the noise caused by laser confocal scanning microscopy :
 
-I = imread("02.tif");
-
-% Remove black margins (crop the image) 
-I_copy = I;
-I_copy(I<15) = 0;
-
-[r, c] = find(I_copy);
-row1 = min(r);
-row2 = max(r);
-col1 = min(c);
-col2 = max(c);
-I_cropped = I(row1:row2, col1:col2);
-imshow(I_cropped);
-[M, N]=size(I_cropped);
-
-% histogramm equalisation: 
-
-I_eq = histeq(I_cropped);
-figure
-subplot(1,3,2);
-imshow(I_cropped);
-subplot(1,3,2);
-imshow(I_cropped);
-subplot(1,3,3);
-imshow(I_eq);
+% Vecteurs contenant les 5 images :
+list_img = dir("../Database1/images/*.tif");
+image_data= cell(1,length(list_img));
+for j=1:length(list_img)
+    file_name = strcat('../Database1/images/', list_img(j).name);
+    image_file=imread(file_name);
+    image_data{j}=image_file;
+end
 
 
-% Separate cells in the foreground  and  cells behind in the "background"
+dir_name = "../Database1/Denoised_images"; % folder name where we'll save detected edges images
+if ~exist(dir_name, 'dir')
+    mkdir(dir_name);
+end
 
 
-level = graythresh(I);% seuillage automatique pour binarisation
-img_bw = im2bw(I,level);% binarisation
+for i=1:length(list_img)
+    
+    I = image_data{i};
 
-figure 
-subplot(1,2,1);
-imshow(I);
-%colormap(gray);
-title ('image en nuances de gris');
+   
+    % Remove black margins (crop the image) 
+    I_copy = I;
+    I_copy(I<15) = 0;
 
-subplot(1,2,2);
-imshow(img_bw);
-%colormap(gray);
-title ('image binarisee en noir et blanc');
-
-
-img2 = bwareaopen(img_bw,150); % pour supprimer les fausses cellules contenant moins de 100 pixels
-
-D = -bwdist(~img2);
-mask = imextendedmin(D,2);
-D2 = imimposemin(D,mask);
-Ld2 = watershed(D2);   % pour séparer les cellules
-img3 = img2;
-img3(Ld2 == 0) = 0;
-
-data_img4 = img3(:,:)>0; % image binaire dont l'intensite des  pixels est de 1 (les blancs)
-[img_labelled, nombreGrains] = bwlabel(data_img4);  % etalonnage  de l'image
+    [r, c] = find(I_copy);
+    row1 = min(r);
+    row2 = max(r);
+    col1 = min(c);
+    col2 = max(c);
+    I_cropped = I(row1:row2, col1:col2);
+    [M, N]=size(I_cropped);
 
 
-figure
+    fig1 = figure
+    subplot(1,2,1);
+    imshow(I);
+    subplot(1,2,2);
+    imshow(I_cropped);
 
-subplot(2,2,1);
-imshow(img2);
-title ('image sans "faux grains"');
+    % Opening - Closing reconstruction : 
+    gmag = imgradient(I_cropped);
+    fig2 = figure
+    subplot(3,3,1);
+    imshow(gmag,[])
+    title('Gradient Magnitude')
 
-subplot(2,2,2);
-imshowpair(img2,mask,'blend')
+    se = strel("disk", 5);
+    Io = imopen(I_cropped,se);
+    subplot(3,3,2);
+    imshow(Io)
+    title('Opening')
 
-subplot(2,2,3);
-imshow(img3);  
-title ('separation des grains')
+    Ie = imerode(I_cropped,se);
+    Iobr = imreconstruct(Ie,I_cropped);
+    subplot(3,3,3);
+    imshow(Iobr)
+    title('Opening-by-Reconstruction')
 
-subplot(2,2,4);
-imshow(img_labelled);
-title ('image etiquettee');
+    Ioc = imclose(Io,se);
+    subplot(3,3,4);
+    imshow(Ioc)
+    title('Opening-Closing')
+
+    Iobrd = imdilate(Iobr,se);
+    Iobrcbr = imreconstruct(imcomplement(Iobrd),imcomplement(Iobr));
+    Iobrcbr = imcomplement(Iobrcbr);
+    subplot(3,3,5);
+    imshow(Iobrcbr);
+    title('Opening-Closing by Reconstruction')
 
 
-% proprietes morphologiques des cellules:
-stats = regionprops (data_img4, 'Area', 'Perimeter', 'Centroid', 'Eccentricity'); 
+    % Contrast Enhancement
+    I_final  =  adapthisteq(Iobrcbr);
+    subplot(3,3,6);
+    imshow(I_final);
 
-M_area = mean([stats.Area]);  % moyenne des aires 
-M_eccentricity = mean([stats.Eccentricity]);  % moyenne des circularite ( 0 = circulaire ; 1 = lineaire)
-M_perimeter = mean([stats.Perimeter]);  % moyenne des perimetres
+    %seuillage automatique pour binarisation with Otsu threshold 
+    [T,EM] = graythresh(I_final);
+    I_bw = im2bw(I_final,T);  % binarisation
+
+    fig3 = figure;
+    subplot(1,3,1);
+    imshow(I_cropped);
+    title("Original image");
+    
+    
+    
+    subplot(1,3,2);
+    imshow(I_final);
+    title("Image after Opening-Closing by Reconstruction");
+    
+    subplot(1,3,3);
+    imshow(I_bw);
+    title("Final denoised and binarized image");
+    
+    img_name = strcat(list_img(i).name(1:2), "_denoised.tif")
+    img_name
+    img_path = strcat("../Database1/Denoised_images/", img_name);
+    img_path
+    imwrite(I_bw,img_path);
 
 
-
-
+end
