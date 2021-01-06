d9fb1e2cae152140953db5c9fce3ccec59c3263a
Titouan <titouan.poquillon@gmail.com>
2021-01-06 18:41:30 +0100

    evaluation complete Signed-off-by: Titouan <titouan.poquillon@gmail.com>

--- Code/Evaluation_worksheet.m
+++ Code/Evaluation_worksheet.m
@@ -1,7 +1,57 @@
 clear all;
 close all;
 
-%% test
+
+%% Ilastick segmentation evaluation
+ id = ["02","04","07","09","16"];
+ T1 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
+ for i = 1:5
+    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
+    I2 = imread("..\Database1\Ilastik\"+id(i)+"_Object Identities.png");
+    c = count_detected_cells(I1, I2);
+    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
+    T1(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
+ end
+T1
+writetable(T1,"..\Evaluation\Ilastik_segmentation_evaluation.csv")
+
+%% Watershed segmentation evaluation
+ id = ["02","04","07","09","16"];
+ T2 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
+ for i = 1:5
+    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
+    I2 = imread("..\Database1\Watershed_segmentation\"+id(i)+"_segmented.tif");
+    c = count_detected_cells(I1, I2);
+    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
+    T2(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
+ end
+T2
+writetable(T2,"..\Evaluation\Watershed_segmentation_evaluation.csv")
+%% Region-growing segmentation evaluation
+ id = ["02","04","07","09","16"];
+ T3 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
+ for i = 1:5
+    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
+    I2 = imread("..\Database1\region_growing\"+id(i)+"_segmented.tif");
+    c = count_detected_cells(I1, I2);
+    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
+    T3(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
+ end
+T3
+writetable(T3,"..\Evaluation\Region_growing_segmentation_evaluation.csv")
+%% K_mean segmentation evaluation
+ id = ["02","04","07","09","16"];
+ T4 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
+ for i = 1:5
+    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
+    I2 = imread("..\Database1\Kmean_segmentation\"+id(i)+"_segmented.tif");
+    c = count_detected_cells(I1, I2);
+    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
+    T4(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
+ end
+T4
+writetable(T4,"..\Evaluation\K_mean_segmentation_evaluation.csv")
+%% test eval
 % importe une image bianaire ref et une image binaire segmentation (pour ce
 % test on utilise deux images mask de références 
 %(Database1\msk\label_02.tif et Database1\msk\label_07.tif")
@@ -19,43 +69,14 @@ c = count_detected_cells(I1, I2)
 % superposition)
 sd = GlobalSorenson_Dice(I1, I2)
 
+%% Test 3dcount++
+id = ["02","04","07","09","16"];
+slices = cell(5,1);
+for i = 1:5
+    slices{i}= imread("..\Database1\Ilastik\"+id(i)+"_Object Identities.png");
+end
+[count, maximum] = Count_cell_slices(slices)
 
-%% Ilastick segmentation testing
- id = ["02","04","07","09","16"];
- T1 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
- for i = 1:5
-    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
-    I2 = imread("..\Database1\Ilastik\"+id(i)+"_Object Identities.png");
-    c = count_detected_cells(I1, I2);
-    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
-    T1(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
- end
-T1
-
-
-%% Watershed segmentation testing
- id = ["02","04","07","09","16"];
- T2 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
- for i = 1:5
-    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
-    I2 = imread("..\Database1\Watershed_segmentation\"+id(i)+"_segmented.tif");
-    c = count_detected_cells(I1, I2);
-    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
-    T2(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
- end
-T2
-
-%% Region-growing segmentation testing
- id = ["02","04","07","09","16"];
- T3 = table('Size',[5, 5], 'VariableTypes',["string","double","double","double","double"],'VariableNames',[ "Image","Global_dice", "Av_dice_per_cell", "Cell_detection_precision", "Cell_detection_sensitivity"]);
- for i = 1:5
-    I1 = imread("..\Database1\msk\label_"+id(i)+".tif");
-    I2 = imread("..\Database1\region_growing\"+id(i)+"_segmented.tif");
-    c = count_detected_cells(I1, I2);
-    [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(I1, I2);
-    T3(i,:) = array2table([id(i), global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity]);
- end
-T3
 %% Functions 
 function count = count_detected_cells(Iref, Iseg)
     count = 0;
@@ -123,4 +144,16 @@ end
 function [ global_dice, av_dice_per_cell, cell_detection_precision, cell_detection_sensitivity] = Eval(Iref, Iseg)
     global_dice = GlobalSorenson_Dice(Iref, Iseg);
     [count, cell_detection_precision, cell_detection_sensitivity, av_dice_per_cell] = Count_Dice_cells(Iref, Iseg);
+end
+
+function [count, maximum] = Count_cell_slices(slices)
+sum_cell = 0;
+for i = 1:length(slices)
+    sum_cell = sum_cell + max(bwlabel(slices{i}),[],"all");
+end
+maximum = sum_cell;
+for i = 1:length(slices)-1
+    sum_cell = sum_cell - 2*count_detected_cells(slices{i}, slices{i+1});
+end
+count = sum_cell;
 end
